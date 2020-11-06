db8e0f89daec5635e9a14f422dd2205a660088e2
Ben <benredekop25@gmail.com>
2020-11-05 22:47:40 -0600

    copied code over from original local project

---
+++ server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+const PATH = __dirname + '/pages/';
+const app = express();
+const bodyParser = require('body-parser');
+ 
+app.use(bodyParser.urlencoded({extended: true}));
+
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: 'CFdb',
+    user: 'root',
+    password: 'admin',
+    database: 'coffeeShop'
+});
+ 
+app.get('/', (req,res) => {
+         res.send('hello world from root!');
+         console.log('from root')
+});
+
+app.get('/select', (req, res) => {
+
+    var sql = 'SELECT * FROM menu';
+    con.query(sql, function (err, result, fields) {
+      if (err) throw err;
+
+      res.json(result);
+    });
+    
+});
+
+app.use('/', express.static('pages'));
+   
+app.listen(PORT, HOST);
+
+console.log('up and running');
